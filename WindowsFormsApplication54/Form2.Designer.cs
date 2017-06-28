ff92711828b58295a27cdb5c10a57a4d4a0540a3
antmen71 <orkunantmen@hotmail.com>
2017-06-28 12:56:10 +0300

    SQL server explorer

---
+++ WindowsFormsApplication54/Form2.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WindowsFormsApplication54
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(188, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(206, 12);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(188, 21);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedValueChanged += new System.EventHandler(this.comboBox2_SelectedValueChanged);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 488);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
---
+++ WindowsFormsApplication54/Form2.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication54
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.ResetText();
+            comboBox2.Items.Clear();
+            SqlConnectionStringBuilder cs = new SqlConnectionStringBuilder();
+            cs[("server")] = ".";
+            cs[("database")] = comboBox1.SelectedItem;
+            cs[("trusted_connection")] = true;
+            SqlConnection cnn = new SqlConnection(cs.ToString());
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM sysobjects where xtype='u'", cnn);
+
+            cnn.Open();
+            SqlDataReader rd = cmd.ExecuteReader();
+            while (rd.Read())
+            {
+                comboBox2.Items.Add(rd[0]);
+            }
+
+            cnn.Close();
+
+
+        }
+        ListView lstvw = new ListView();
+
+
+
+
+        private void comboBox2_SelectedValueChanged(object sender, EventArgs e)
+        {
+
+            lstvw.Columns.Clear();
+            lstvw.Items.Clear();
+            SqlConnectionStringBuilder cs = new SqlConnectionStringBuilder();
+            cs[("server")] = ".";
+            cs[("database")] = comboBox1.Text;
+            cs[("trusted_connection")] = true;
+            SqlConnection cnn = new SqlConnection(cs.ToString());
+
+            SqlCommand cmd = new SqlCommand("SELECT COUNT(*)  FROM syscolumns JOIN sysobjects ON sysobjects.id = syscolumns.id WHERE sysobjects.xtype='U' AND sysobjects.name =@tblname", cnn);
+
+            SqlCommand cmd1 = new SqlCommand("SELECT * FROM syscolumns JOIN sysobjects ON sysobjects.id = syscolumns.id WHERE sysobjects.xtype='U' AND sysobjects.name =@tblname  ", cnn);
+            
+            cnn.Open();
+            cmd.Parameters.AddWithValue("@tblname", comboBox2.SelectedItem);
+            cmd1.Parameters.AddWithValue("@tblname", comboBox2.SelectedItem);
+            
+            Int32 count = (Int32)cmd.ExecuteScalar();
+            SqlDataReader rd = cmd1.ExecuteReader();
+
+            lstvw.View = View.Details;
+            lstvw.FullRowSelect = true;
+            lstvw.GridLines = true;
+            lstvw.Left = 12;
+            lstvw.Top = 40;
+            lstvw.Width = this.Width - 40;
+            lstvw.Height = this.Height - 100;
+            lstvw.BringToFront();
+            lstvw.Anchor = (AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top);
+            lstvw.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+            this.Controls.Add(lstvw);
+
+            while (rd.Read())
+            {
+                lstvw.Columns.Add(rd[0].ToString());
+            }
+
+            rd.Close();
+
+            string tabloIsmi = comboBox2.SelectedItem.ToString();
+            if (tabloIsmi.Contains(" "))
+            {
+                tabloIsmi = tabloIsmi.PadLeft(tabloIsmi.Length + 1, '[');
+                tabloIsmi = tabloIsmi.PadRight(tabloIsmi.Length + 1, ']');
+            }
+            string queryString = "SELECT * FROM " + tabloIsmi;
+            SqlCommand cmd2 = new SqlCommand(queryString, cnn);
+
+
+            SqlDataReader rd1 = cmd2.ExecuteReader();
+
+            while (rd1.Read())
+            {
+                ListViewItem itm = new ListViewItem();
+                itm.Text = rd1[0].ToString();
+                for (int k = 1; k < count; k++)
+                {
+                    itm.SubItems.Add(rd1[k].ToString());
+
+                }
+                lstvw.Items.Add(itm);
+
+            }
+
+            cnn.Close();
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            SqlConnection cnn = new SqlConnection("server=.;database=master;trusted_connection=yes");
+            SqlCommand cmd = new SqlCommand("select name from sysdatabases", cnn);
+
+            cnn.Open();
+
+            SqlDataReader rd = cmd.ExecuteReader();
+
+            while (rd.Read())
+            {
+                comboBox1.Items.Add(rd[0]);
+            }
+            cnn.Close();
+
+        }
+    }
+}
